aed579442bcfbd90b8df6ce73298661d15b82829
Miofly <953212389@qq.com>
2021-01-08 18:07:43 +0800

    1

--- packages/xcx_lottery/src/pages/index/index.vue
+++ packages/xcx_lottery/src/pages/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <view style="overflow-x: hidden;overflow-y: scroll;" class="text-center full-width-height">
+    <view style="overflow-x: hidden;overflow-y: scroll" class="text-center full-width-height">
         <view style="min-height: 100vh;height: auto;" class="text-center full-width">
             <!-- 背景 -->
             <m-image :borderRadius="10" :mode="['aspectFit', 'scaleToFill', 'aspectFill', 'widthFix', 'heightFix'][3]" :shape="['square', 'circle'][0]"
@@ -92,6 +92,64 @@
                           borderRadius="10" style="position: relative;z-index: 3;"
                           color="rgba(255, 245, 150, 1)" mode="vertical">
             </m-column-notice>
+
+            <!--我的奖品-->
+            <view style="position: relative" class="margin-top">
+                <m-image duration="0" :showLoading="false" :borderRadius="10" bgColorError="rgba(0, 0, 0, 1)" height="336"
+                		:mode="['aspectFit', 'scaleToFill', 'aspectFill', 'widthFix', 'heightFix'][4]"
+                		:shape="['square', 'circle'][0]" :src="baseConfig.wdjp" bgColor="rgba(0, 0, 0, 1)">
+                	<view slot="error" style="font-size: 24rpx;" class="text-white">加载失败</view>
+                </m-image>
+                <view style="position: absolute;left: 112rpx;top: 280rpx">
+                    <line-progress :percent="(userData.phoneNum/30)*100" percentColor="black"
+                                   borderStyle="0 solid red"
+                                   percentSize="15px" :striped="true" :stripedActive="true" inactiveColor="rgba(255, 214, 180, 1)"
+                                   :showPercent="true">
+                    </line-progress>
+                    <text style="margin-left: 8rpx;color: rgba(231, 79, 0, 1);vertical-align: top"><span style="color: rgba(231, 79, 0, 1);" class="text-14 text-bold">{{userData.phoneNum}}</span><span style="font-size: 24rpx;transform: scale(0.8)">/30</span></text>
+                </view>
+
+                <view style="position: absolute;left: 456rpx;top: 280rpx">
+                    <line-progress :percent="(userData.vipNum/10)*100" percentColor="black"
+                                   borderStyle="0 solid red"
+                                   percentSize="15px" :striped="true" :stripedActive="true" inactiveColor="rgba(255, 214, 180, 1)"
+                                   :showPercent="true">
+                    </line-progress>
+                    <text style="margin-left: 8rpx;color: rgba(231, 79, 0, 1);vertical-align: top"><span style="" class="text-14 text-bold">{{userData.vipNum}}</span><span style="font-size: 24rpx;transform: scale(0.8);color: rgba(231, 79, 0, 1);">/10</span></text>
+                </view>
+            </view>
+
+            <!--中奖晒单-->
+            <view style="background: rgba(255, 243, 237, 1);width: 690rpx;height: 300px;margin-top: 56rpx;position: relative;padding-bottom: 32rpx;border-radius: 16rpx"
+                  class="text-center margin-center">
+                <m-image duration="0" :showLoading="false" :borderRadius="10" bgColorError="rgba(0, 0, 0, 1)" height="80"
+                		:mode="['aspectFit', 'scaleToFill', 'aspectFill', 'widthFix', 'heightFix'][4]" style="position: absolute;top: -16rpx"
+                		:shape="['square', 'circle'][0]" :src="baseConfig.zjsd" bgColor="rgba(0, 0, 0, 1)">
+                	<view slot="error" style="font-size: 24rpx;" class="text-white">加载失败</view>
+                </m-image>
+                <view class="margin-center" style="height: 150rpx;width: 690rpx;position: relative;top: 96rpx;
+            background: linear-gradient(90deg, rgba(255, 255, 255, 0.34) 0%, rgba(255, 255, 255, 0.2) 100%);
+            box-shadow: 1px 1px 2px 0px rgba(255, 255, 255, 0.23);border-radius: 8px;">
+                    <!--头像-->
+                    <view class="cu-avatar" :class="[false ? 'radius' : 'round']"
+                          :style="{backgroundImage: 'url('+ userData.avatar +')'}" style="width: 68rpx;height: 68rpx;position: absolute;top: 28rpx;left: 32rpx">
+                        <view v-if="false" class="cu-tag badge">999</view>
+                    </view>
+                    <!--用户信息-->
+                    <view class=" full-width" style="color: rgba(73, 54, 43, 1)">
+                        <view style="position: absolute;top: 20rpx;left: 124rpx;color: rgba(73, 54, 43, 1)" class="text-16 text-bold">{{userData.name}}</view>
+                        <view style="position: absolute;top: 68rpx;left: 124rpx">
+
+                            <text class="text-12" style="color: rgba(73, 54, 43, 0.5);">合肥市包河区宝文中心</text>
+                        </view>
+                    </view>
+                    <!--中奖纪录按钮-->
+                    <view style="position: absolute;top: 0;right: 28rpx">
+                        <image :mode="['aspectFit', 'scaleToFill', 'aspectFill', 'widthFix', 'heightFix'][3]"
+                               :src="baseConfig.zjjl" style="width: 84rpx"></image>
+                    </view>
+                </view>
+            </view>
         </view>
         <m-modal :closeShow="true" :closeSize="40" :descSize="30" :maskClosable="false" :showContent="true"
                  :showTitle="false" :status.sync="modalStatus"
@@ -124,18 +182,18 @@
                 </view>
             </view>
         </m-modal>
-
-
     </view>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import mColumnNotice from '../../components/m-column-notice/m-column-notice.vue'
+import lineProgress from '../../components/progress/line-progress.vue'
 
 @Component({
     components: {
-        mColumnNotice
+        mColumnNotice,
+        lineProgress
     },
 })
 export default class extends Vue {
@@ -144,6 +202,9 @@ export default class extends Vue {
         zjjl: '/static/images/zjjl.png',
         bt: '/static/images/bt.png',
         btn: '/static/images/btn.png',
+        wdjp: '/static/images/wdjp.png',
+        zjsd: '/static/images/zjsd.png',
+        wfsm: '/static/images/wfsm.png',
 
         baseSrc: 'https://6d69-miofly-k1xjk-1303051262.tcb.qcloud.la/images/glnz/1.jpg',
         bg: '/static/images/bg.png',
@@ -189,6 +250,8 @@ export default class extends Vue {
         over: '3400',
         name: '忽而个规格',
         num: 5,
+        phoneNum: 0,
+        vipNum: 0,
     }
 
     modalStatus: boolean = false
