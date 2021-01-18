e2e509530bfc4a036c31f801ec00f36c14c3de73
Miofly <953212389@qq.com>
2021-01-18 20:36:13 +0800

    1

--- mock/users.ts
+++ mock/users.ts
@@ -8,212 +8,121 @@ const adminInfo = {
 	real_name: 'admin',
 	role_id: 1,
 	username: 'admin',
+	name: 'admin',
+	avatar: 'https://6d69-miofly-k1xjk-1303051262.tcb.qcloud.la/images/glnz/1.jpg',
+	introduction: '1'
 }
 
 const auth_list = [
 	{
-		id: 106,
-		auth_parent_id: 0,
-		sort: 1,
-		icon: 'icon-yundanjifei',
-		auth_name: '图片管理',
-		url: '',
-		son: [
+		id: 1,
+		name: '首页',
+		pid: 0,
+		route: '',
+		icon: 'iconhome',
+		key: '1.00',
+		created_at: '2019-05-30 13:05:04',
+		updated_at: '2020-12-21 14:23:44',
+		sub_permission: [
 			{
-				id: 107,
-				auth_parent_id: 106,
-				sort: 1,
+				id: 1,
+				name: '首页',
+				pid: 1,
+				route: 'home',
 				icon: '',
-				auth_name: '图片管理',
-				url: 'img',
-				key: 'group107'
+				key: '0.00',
+				created_at: '2019-05-30 13:06:18',
+				updated_at: '2020-12-21 11:37:47'
 			}
-		],
-		key: 'group106'
+		]
 	},
 	{
-		id: 108,
-		auth_parent_id: 0,
-		sort: 11,
-		icon: 'icon-yonghuguanli1',
-		auth_name: '订阅号管理',
-		url: '',
-		son: [
+		id: 2,
+		name: '小程序',
+		pid: 0,
+		route: 'xcx',
+		icon: 'iconxiaochengxu',
+		key: '1.00',
+		created_at: '2019-05-30 13:05:04',
+		updated_at: '2020-12-21 14:23:44',
+		sub_permission: [
 			{
-				id: 109,
-				auth_parent_id: 108,
-				sort: 1,
+				id: 1,
+				name: '大转盘',
+				pid: 2,
+				route: 'turntable',
 				icon: '',
-				auth_name: '订阅号管理',
-				url: 'dyh',
-				key: 'group109'
+				key: '0.00',
+				created_at: '2019-05-30 13:06:18',
+				updated_at: '2020-12-21 11:37:47'
 			}
-		],
-		key: 'group108'
+		]
 	},
 	{
-		id: 83,
-		auth_parent_id: 0,
-		sort: 111,
-		icon: 'icon-svgwrite',
-		auth_name: '会员管理',
-		url: '',
-		son: [
+		id: 3,
+		name: '权限设置',
+		pid: 0,
+		route: 'backend',
+		icon: 'iconquanxian',
+		key: '6.00',
+		created_at: '2019-05-30 13:12:10',
+		updated_at: '2020-03-24 13:46:42',
+		sub_permission: [
 			{
-				id: 89,
-				auth_parent_id: 83,
-				sort: 1,
-				icon: '',
-				auth_name: '会员审核列表',
-				url: 'member-list',
-				key: 'group89'
+				id: 1,
+				name: '管理员列表',
+				pid: 3,
+				route: 'administrator-list',
+				icon: null,
+				key: null,
+				created_at: '2019-05-28 17:50:22',
+				updated_at: '2020-02-12 16:46:42'
 			},
 			{
-				id: 88,
-				auth_parent_id: 83,
-				sort: 1,
+				id: 2,
+				name: '角色管理',
+				pid: 3,
+				route: 'management-group-list',
 				icon: '',
-				auth_name: '会员列表',
-				url: 'member',
-				key: 'group88'
+				key: '0.00',
+				created_at: '2019-05-30 13:47:52',
+				updated_at: '2020-03-24 14:07:08'
 			},
 			{
-				id: 99,
-				auth_parent_id: 83,
-				sort: 3,
+				id: 113,
+				name: '权限管理',
+				pid: 3,
+				route: 'permissions-list',
 				icon: '',
-				auth_name: '会员产品关联',
-				url: 'manage',
-				key: 'group99'
+				key: '0.00',
+				created_at: '2019-05-30 13:48:04',
+				updated_at: '2020-02-12 16:46:25',
+				sub_permission: [
+					{
+						id: 10,
+						name: '大转盘',
+						pid: 113,
+						route: 'turntable',
+						icon: '',
+						key: '0.00',
+						created_at: '2019-05-30 13:06:18',
+						updated_at: '2020-12-21 11:37:47'
+					}
+				]
 			}
-		],
-		key: 'group83'
+		]
 	},
-	{
-		id: 84,
-		auth_parent_id: 0,
-		sort: 112,
-		icon: 'icon-shouye',
-		auth_name: '内容管理',
-		url: '',
-		son: [
-			{
-				id: 85,
-				auth_parent_id: 84,
-				sort: 1,
-				icon: '',
-				auth_name: '转发产品列表',
-				url: 'article',
-				key: 'group85'
-			}
-		],
-		key: 'group84'
-	},
-	{
-		id: 86,
-		auth_parent_id: 0,
-		sort: 113,
-		icon: 'icon-Configurationmanage',
-		auth_name: '配置管理',
-		url: '',
-		son: [
-			{
-				id: 87,
-				auth_parent_id: 86,
-				sort: 1,
-				icon: '',
-				auth_name: '配置详细信息',
-				url: 'index-settings',
-				key: 'group87'
-			}
-		],
-		key: 'group86'
-	},
-	{
-		id: 95,
-		auth_parent_id: 0,
-		sort: 114,
-		icon: 'icon-yundanjifei',
-		auth_name: '团队管理',
-		url: '',
-		son: [
-			{
-				id: 96,
-				auth_parent_id: 95,
-				sort: 1,
-				icon: '',
-				auth_name: '团队成员',
-				url: 'team-member',
-				key: 'group96'
-			},
-			{
-				id: 97,
-				auth_parent_id: 95,
-				sort: 2,
-				icon: '',
-				auth_name: '团队点击列表',
-				url: 'team-click',
-				key: 'group97'
-			},
-			{
-				id: 98,
-				auth_parent_id: 95,
-				sort: 3,
-				icon: '',
-				auth_name: '团队结算',
-				url: 'team-settlement',
-				key: 'group98'
-			}
-		],
-		key: 'group95'
-	},
-	{
-		id: 7,
-		auth_parent_id: 0,
-		sort: 115,
-		icon: 'icon-guanliyuanguanli',
-		auth_name: '后台管理',
-		url: 'root_manage',
-		son: [
-			{
-				id: 8,
-				auth_parent_id: 7,
-				sort: 1,
-				icon: '',
-				auth_name: '用户管理',
-				url: 'account',
-				key: 'group8'
-			},
-			{
-				id: 9,
-				auth_parent_id: 7,
-				sort: 2,
-				icon: '',
-				auth_name: '角色管理',
-				url: 'role',
-				key: 'group9'
-			},
-			{
-				id: 10,
-				auth_parent_id: 7,
-				sort: 3,
-				icon: '',
-				auth_name: '权限管理',
-				url: 'auth',
-				key: 'group10'
-			}
-		],
-		key: 'group7'
-	}
 ]
 
+// 登录接口
 export const login = (req: Request, res: Response) => {
+	console.log(req.body)
 	const { username } = req.body
 	if (adminInfo.username === username) {
 		return res.json({
 			code: 200,
 			data: {
-				authToken: '4fef5fe81c28ac77b602f5fb614b2f43',
+				access_token: '4fef5fe81c28ac77b602f5fb614b2f43',
 				expire: 14400
 			}
 		})
@@ -224,11 +133,13 @@ export const login = (req: Request, res: Response) => {
 	})
 }
 
+// 用户信息接口
 export const getLoginInfo = (req: Request, res: Response) => res.json({
 	code: 200,
 	data: adminInfo
 })
 
+// 用户权限接口
 export const getAuthList = (req: Request, res: Response) => res.json({
 	code: 200,
 	data: {
